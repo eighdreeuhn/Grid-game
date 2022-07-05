1a88753bf1367be21d9805991c0490cf59db7e62
Adrian Gioia <afgioia@gmail.com>
2022-07-05 14:23:36 -0700

    Added inversion counting function to test for solvability & solved the re-render problem for the winner screen.

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,11 @@
+
+const Welcome = () => {
+    return (
+        <div className='message'>
+            <h2>Number Tiles!</h2>
+            <h4>Click the tiles to move them and order them from 1 - 24</h4>
+        </div>
+    )
+}
+
+export default Welcome
---
+++ src/Components/Winner.jsx
@@ -0,0 +1,8 @@
+const Winner = () => {
+  return (
+    <div>
+      <h3>Congratulations!</h3>
+    </div>
+  );
+};
+export default Winner;
